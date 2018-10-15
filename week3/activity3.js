03ec6d64f7d862351c12269ed15f6cca2af1db93
Casey Boettcher <boettchc@oregonstate.edu>
2018-10-15 04:04:33 -0400

    Finished activity3.js, but it's ugly

--- week3/activity3.js
+++ week3/activity3.js
@@ -1,5 +1,7 @@
-function deepEqual(x, y)
+/*function deepEqual(x, y)
 {
+    if( x == null && y == null)
+        return true;
     if (x == null &! y == null)
         return false;
     else if (typeof(x) != typeof(y) )
@@ -25,9 +27,41 @@ function deepEqual(x, y)
         }
     }
 }
+*/
 
-//let x = "3";
-//let y = 3;
+function deepEqual(x, y)
+{
+    let flag = true;
+    function deeper(x, y)
+    { 
+        let tX = typeof(x);
+        let tY = typeof(y);
+        if(tX != tY || (tX != 'object' && x !== y))
+            flag = false; 
+        else if(tX == tY)
+        {
+            if(tX == 'object' && x != null)
+            {
+                let xKeys = Object.keys(x);
+                let yKeys = Object.keys(x);
+                if(xKeys.length == yKeys.length)
+                {
+                    for(let k in x)
+                    {
+                        deeper(x[k], y[k]);
+                    }
+                }
+            }
+            else
+            {
+                if(flag)
+                    flag = (x === y);
+            }
+        }
+    }
+    deeper(x, y);
+    return flag;
+}
 
 let obj1 = 
 {
@@ -36,7 +70,7 @@ let obj1 =
       guid: '{{guid()}}',
       isActive: '{{bool()}}',
       balance: '{{floating(1000, 4000, 2, "$0,0.00")}}',
-      gender: { key1: "suck4", key2: "boy", key3: "GreenGoblin"},
+      gender: { key1: "sucka", key2: "boy", key3: "GreenGoblin"},
       picture: 'http://placehold.it/32x32',
       age: '{{integer(20, 40)}}'
 };
@@ -51,41 +85,22 @@ let obj2 =
       gender: { key1: "sucka", key2: "boy", key3: "GreenGoblin"},
       age: '{{integer(20, 40)}}'
 };
-
-
-
 let obj3 = 
-{     eyeColor: '{{random("blue", "brown", "green")}}',
-      name: '{{firstName()}} {{surname()}}',
+{
+      picture: 'http://placehold.it/32x32',
       gender: { key1: "sucka", key2: "boy", key3: "GreenGoblin"},
-      company: '{{company().toUpperCase()}}',
-      email: '{{email()}}',
-      phone: '+1 {{phone()}}',
+      age: '{{integer(20, 40)}}'
 };
-/* 
-      latitude: '{{floating(-90.000001, 90)}}',
-      longitude: '{{floating(-180.000001, 180)}}',
-      tags: [
-        '{{repeat(7)}}',
-        '{{lorem(1, "words")}}'
-      ],
-      friends: [
-        '{{repeat(3)}}',
-        {
-          id: '{{index()}}',
-          name: '{{firstName()}} {{surname()}}'
-        }
-      ],
-      greeting: function (tags) {
-        return 'Hello, ' + this.name + '! You have ' + tags.integer(1, 10) + ' unread messages.';
-      },
-      favoriteFruit: function (tags) {
-        var fruits = ['apple', 'banana', 'strawberry'];
-        return fruits[tags.integer(0, fruits.length - 1)];
-      }
-    }
- */
 
-//console.log(deepEqual(x, y));
+let n1 = null;
+let n2 = null;
+
+let nan1 = NaN;
+let nan2 = NaN;
+
 console.log(deepEqual(obj1, obj2));
-//console.log(deepEqual(x, y));
+console.log(deepEqual(n1, n2));
+console.log(deepEqual(n1, obj3));
+console.log(deepEqual(nan1, nan2));
+console.log(Date.now());
+
