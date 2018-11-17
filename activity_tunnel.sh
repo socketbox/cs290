d12cf7d271814f815973067bbdd6f380ab37722a
Casey Boettcher <boettchc@oregonstate.edu>
2018-11-17 06:35:34 -0500

    Adding nodemailer to project deps

---
+++ activity_tunnel.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ssh -f -N -L 44332:localhost:44332 boettchc@flip2.engr.oregonstate.edu
---
+++ project_tunnel.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ssh -f -N -L 32323:localhost:32323 boettchc@flip2.engr.oregonstate.edu
